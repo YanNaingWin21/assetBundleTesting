b57441d09457224a8990370cd147ab8aa36c41f8
yannaingwin <yan.naing.win@studioamk.com>
2020-08-07 04:58:21 +0630

    Added Project

---
+++ assets/mainController.js
@@ -0,0 +1,50 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        cc.assetManager.loadBundle('colorAssemble', function (err, bundle) {
+            if (err) {
+                return console.error(err);
+            }
+
+            bundle.loadScene('colorAssemble', function (err, scene) {
+                cc.director.runScene(scene);
+            });
+            console.log('load bundle successfully.');
+        });
+
+    },
+
+    start() {
+
+    },
+
+    // update (dt) {},
+});
